9b57eaa07161664a3dcb7e4585d7df5f7429f93f
Richard Michael Blay <rblay30@gmail.com>
2024-01-24 11:35:35 +0100

    doing cool stuff with NHANES

--- R/learning.R
+++ R/learning.R
@@ -7,3 +7,49 @@ r3::check_git_config()
 usethis::gh_token_help()
 usethis::create_github_token()
 usethis::use_github()
+library(tidyverse)
+library(NHANES)
+glimpse(NHANES)
+str(NHANES)
+
+# Select specific columns -------------------------------------------------
+
+select(NHANES, Age, Weight, BMI)
+select(NHANES, -HeadCirc)
+select(NHANES, starts_with("BP"))
+select(NHANES, ends_with("Day"))
+select(NHANES, contains("Age"))
+
+
+
+# Rename all columns ------------------------------------------------------
+
+NHANES_small <- rename_with(NHANES, snakecase::to_snake_case)
+
+
+# renaming specific column ------------------------------------------------
+
+
+NHANES_small <- rename(NHANES_small, sex = gender)
+
+# Chaining the functions with the pipe ------------------------------------
+
+colnames(NHANES_small)
+NHANES_small %>%
+    colnames()
+NHANES_phys <- NHANES_small %>%
+    select(phys_active) %>%
+    rename(physically_active = phys_active)
+
+# exercise ----------------------------------------------------------------
+
+NHANES_small %>%
+    select(bp_sys_ave, education)
+NHANES_small %>%
+    rename(bp_sys = bp_sys_ave, bp_dia = bp_dia_ave)
+NHANES_small %>%
+    select(bmi) %>%
+    select(contains("age"))
+NHANES_small %>%
+    rename(bp_systolic = bp_sys_ave)
+
