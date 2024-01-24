1718c58fbef12e1260e7eb7aba1798a1e8a551bf
Richard Michael Blay <rblay30@gmail.com>
2024-01-24 15:59:00 +0100

    new

--- R/learning.R
+++ R/learning.R
@@ -134,3 +134,36 @@ NHANES_modified %>%
     ) %>%
     select(young_child)
 
+# Calculating summary statistics ------------------------------------------
+
+NHANES_small %>%
+    summarise(max_bmi = max(bmi, na.rm = TRUE))
+
+result <- NHANES_small %>%
+    summarise(mean_age = mean(age, na.rm = TRUE),
+              mean_bmi = mean(bmi, na.rm = TRUE))
+
+
+
+# Summary statistics by groups --------------------------------------------
+result <- NHANES_small %>%
+    summarise(mean_age = mean(age, na.rm = TRUE),
+              mean_bmi = mean(bmi, na.rm = TRUE))
+NHANES_small %>%
+    filter(!is.na(diabetes)) %>%
+    group_by(diabetes) %>%
+    summarise(mean_age = mean(age, na.rm = TRUE),
+              mean_bmi = mean(bmi, na.rm = TRUE))
+
+NHANES_small %>%
+    filter(!is.na(diabetes)) %>%
+    group_by(diabetes, phys_active) %>%
+    summarise(mean_age = mean(age, na.rm = TRUE),
+              mean_bmi = mean(bmi, na.rm = TRUE)) %>%
+    ungroup()
+
+# Saving Dataset as a file ------------------------------------------------
+
+readr::write_csv(NHANES_small, here::here("data/NHANES_small_csv")
+)
+r3::open_feedback_survey()
